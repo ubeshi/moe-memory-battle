98bd66808c5282e53cca4160ed5d36873a11e7e8
Colin Tong <tongcolin@live.ca>
2022-03-12 01:10:39 -0500

    Add card flipping

--- src/client/app/memory-game/memory-game-board/memory-game-board.component.ts
+++ src/client/app/memory-game/memory-game-board/memory-game-board.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { SafeUrl } from "@angular/platform-browser";
 import { WaifuApiService } from "@client/app/services/waifu-api/waifu-api.service";
+import { CardFace } from "../memory-game-constants";
 
 const enum MemoryGameState {
   NOT_STARTED,
   STARTED,
   FINISHED,
 };
+
+interface Card {
+  image: SafeUrl;
+  shownFace: CardFace;
+}
 
 @Component({
   selector: "memory-game-board",
@@ -17,13 +23,41 @@ export class MemoryGameBoardComponent implements OnInit {
 
   constructor(private waifuApiService: WaifuApiService) { }
   
-  public cardImages: SafeUrl[] = [];
+  public cards: Card[] = [];
 
   async ngOnInit(): Promise<void> {
-    await this.getNewCards();
+    this.cards = await this.getNewCards();
   }
 
-  async getNewCards() {
-    this.cardImages = await this.waifuApiService.getWaifus();
+  async getNewCards(): Promise<Card[]> {
+    const uniqueCardImages = await this.waifuApiService.getWaifus();
+
+    // Create pairs of cards
+    const uniqueCards: Card[] = uniqueCardImages.map((image) => ({ image, shownFace: CardFace.BACK }));
+    const uniqueCardDupes: Card[] = uniqueCardImages.map((image) => ({ image, shownFace: CardFace.BACK }));
+    const pairedCards = uniqueCards.concat(uniqueCardDupes);
+
+    return this.shuffleCards(pairedCards);
+  }
+
+  private shuffleCards(cards: Card[]): Card[] {
+    cards = cards.slice();
+    for (let index = cards.length - 1; index > 0; index--) {
+      const randomIndex = Math.floor(Math.random() * (index + 1));
+      [cards[index], cards[randomIndex]] = [cards[randomIndex], cards[index]]; // swap
+    }
+    return cards;
+  }
+
+  private flipCard(card: Card) {
+    if (card.shownFace === CardFace.BACK) {
+      card.shownFace = CardFace.FRONT;
+    } else {
+      card.shownFace = CardFace.BACK;
+    }
+  }
+
+  public handleCardClicked(card: Card): void {
+    this.flipCard(card);
   }
 }
--- src/client/app/memory-game/memory-game-card/memory-game-card.component.ts
+++ src/client/app/memory-game/memory-game-card/memory-game-card.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from "@angular/core";
 import { SafeUrl } from "@angular/platform-browser";
+import { CardFace } from "../memory-game-constants";
 
 @Component({
   selector: "memory-game-card",
   templateUrl: "./memory-game-card.component.html",
-  // styleUrls: ["./memory-game-card.component.scss"],
+  styleUrls: ["./memory-game-card.component.scss"],
 })
 export class MemoryGameCardComponent {
   @Input() frontImage: SafeUrl | undefined;
   @Input() backImage: SafeUrl | undefined;
+  @Input() shownFace: CardFace = CardFace.BACK;
+
+  public CardFace = CardFace;
 }
