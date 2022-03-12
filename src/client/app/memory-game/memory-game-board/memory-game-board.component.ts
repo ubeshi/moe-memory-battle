f07538373b373d60faa244d67f0a9d31a68e4d90
Colin Tong <tongcolin@live.ca>
2022-03-12 02:14:00 -0500

    Basic memory game

--- src/client/app/memory-game/memory-game-board/memory-game-board.component.ts
+++ src/client/app/memory-game/memory-game-board/memory-game-board.component.ts
@@ -4,9 +4,9 @@ import { WaifuApiService } from "@client/app/services/waifu-api/waifu-api.servic
 import { CardFace } from "../memory-game-constants";
 
 const enum MemoryGameState {
-  NOT_STARTED,
-  STARTED,
-  FINISHED,
+  NO_CARDS_REVEALED,
+  FIRST_CARD_REVEALED,
+  SECOND_CARD_REVEALED,
 };
 
 interface Card {
@@ -14,19 +14,24 @@ interface Card {
   shownFace: CardFace;
 }
 
+type Deck = Array<Card | null>;
+
 @Component({
   selector: "memory-game-board",
   templateUrl: "./memory-game-board.component.html",
-  // styleUrls: ["./memory-game-board.component.scss"],
+  styleUrls: ["./memory-game-board.component.scss"],
 })
 export class MemoryGameBoardComponent implements OnInit {
 
   constructor(private waifuApiService: WaifuApiService) { }
-  
-  public cards: Card[] = [];
+
+  private memoryGameState = MemoryGameState.NO_CARDS_REVEALED;
+  private firstFlippedCard: Card | undefined;
+
+  public deck: Deck = [];
 
   async ngOnInit(): Promise<void> {
-    this.cards = await this.getNewCards();
+    this.deck = await this.getNewCards();
   }
 
   async getNewCards(): Promise<Card[]> {
@@ -37,19 +42,19 @@ export class MemoryGameBoardComponent implements OnInit {
     const uniqueCardDupes: Card[] = uniqueCardImages.map((image) => ({ image, shownFace: CardFace.BACK }));
     const pairedCards = uniqueCards.concat(uniqueCardDupes);
 
-    return this.shuffleCards(pairedCards);
+    return this.shuffleArray(pairedCards);
   }
 
-  private shuffleCards(cards: Card[]): Card[] {
-    cards = cards.slice();
-    for (let index = cards.length - 1; index > 0; index--) {
+  private shuffleArray<T>(array: Array<T>): Array<T> {
+    array = array.slice();
+    for (let index = array.length - 1; index > 0; index--) {
       const randomIndex = Math.floor(Math.random() * (index + 1));
-      [cards[index], cards[randomIndex]] = [cards[randomIndex], cards[index]]; // swap
+      [array[index], array[randomIndex]] = [array[randomIndex], array[index]]; // swap
     }
-    return cards;
+    return array;
   }
 
-  private flipCard(card: Card) {
+  private flipCard(card: Card): void {
     if (card.shownFace === CardFace.BACK) {
       card.shownFace = CardFace.FRONT;
     } else {
@@ -57,7 +62,59 @@ export class MemoryGameBoardComponent implements OnInit {
     }
   }
 
-  public handleCardClicked(card: Card): void {
-    this.flipCard(card);
+  public handleCardClicked(clickedCard: Card): void {
+    if (clickedCard.shownFace === CardFace.FRONT) {
+      return;
+    }
+
+    switch (this.memoryGameState) {
+      case MemoryGameState.NO_CARDS_REVEALED: {
+        this.handleNoCardsRevealedState(clickedCard);
+        break;
+      }
+      case MemoryGameState.FIRST_CARD_REVEALED: {
+        this.handleFirstCardRevealedState(clickedCard);
+        break;
+      }
+      case MemoryGameState.SECOND_CARD_REVEALED: {
+        // Do nothing
+        break;
+      }
+    }
+  }
+
+  private handleNoCardsRevealedState(clickedCard: Card): void {
+    this.flipCard(clickedCard);
+    this.firstFlippedCard = clickedCard;
+    this.memoryGameState = MemoryGameState.FIRST_CARD_REVEALED;
+  }
+
+  private handleFirstCardRevealedState(clickedCard: Card): void {
+    this.flipCard(clickedCard);
+    this.memoryGameState = MemoryGameState.SECOND_CARD_REVEALED;
+
+    const timeout = setTimeout(() => {
+      clearTimeout(timeout);
+
+      const isClickedCardMatching = clickedCard.image === this.firstFlippedCard?.image;
+      if (isClickedCardMatching) { // Remove matched cards from the deck
+        this.deck = this.removePairFromDeckByImage(this.deck, clickedCard.image);
+      } else {
+        this.flipCard(clickedCard);
+        if (this.firstFlippedCard) {
+          this.flipCard(this.firstFlippedCard);
+        }
+      }
+      this.memoryGameState = MemoryGameState.NO_CARDS_REVEALED;
+    }, 1000);  
+  }
+
+  private removePairFromDeckByImage(deck: Deck, image: SafeUrl): Deck {
+    deck = deck.slice();
+    const firstCardIndex = deck.findIndex((card) => card?.image === image);
+    deck[firstCardIndex] = null;
+    const secondCardIndex = deck.findIndex((card) => card?.image === image);
+    deck[secondCardIndex] = null;
+    return deck;
   }
 }
