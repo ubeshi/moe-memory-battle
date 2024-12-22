11269044f2ed939dc280034c3bacdb8673abb685
ColinT <tongcolin@live.ca>
2024-12-21 23:13:26 -0500

    angular 16 checkpoint

--- src/server/spawn-client.ts
+++ src/server/spawn-client.ts
@@ -4,7 +4,7 @@ import { downloadChromium } from "./download-chromium";
 
 export async function spawnClient(port: number): Promise<void> {
   const installedPath = await downloadChromium({ revision: "1399578", installPath: os.tmpdir(), log: true });
-  const clientProcess = execFile(installedPath, [ `--app=http://localhost:${port}` ], (error) => {
+  const clientProcess = execFile(installedPath, [ `--app=http://localhost:${port}`, `--test-type` ], (error) => {
     if (error) {
       console.error(error);
       throw error;
